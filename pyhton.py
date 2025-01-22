91396bf97db03eb510b601bf0a45f39f12439b12
Pavani-Gunda <pavanigunda23@gmail.com>
2025-01-22 15:50:07 +0530

    Add files via upload

---
+++ pyhton.py
@@ -0,0 +1,34 @@
+#mathematical operations
+#operations using numbers
+print(23+56)
+print(65-23)
+print(56*89)
+print(96/7)
+print(34**2)
+print(96//7)
+print(type(96//7)) # class int
+
+#using variables
+num1=24
+num2=36
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num2/num1)
+print(num1**num2)
+print(num2//num1)
+
+#strings
+print("good morning")
+print('glad to see you')
+hello="nice to meet you"
+print(hello)
+print(len(hello))
+#index and slicing
+hello= "Difficulties in life are intended to make us better, not bitter"
+print(len(hello))
+print(hello[5])
+print(hello[16])
+print(hello[5: 54])
+print(hello[15: 25])
+print(hello[23: 57])
